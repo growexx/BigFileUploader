2566eb7006580b38aad3ecefb925c4be6a07a10f
harshg-growwxxer <harah.gondaliya@growexx.com>
2024-09-09 19:14:19 +0530

    added UI components

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import FileUploadComponent from './src/screens/UploadScreen';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => FileUploadComponent);
