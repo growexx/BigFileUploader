5c4debd2087f2b577cfd8fe88f70ff6e075d7bd4
harshg-growwxxer <harah.gondaliya@growexx.com>
2024-09-10 12:25:34 +0530

    added .env

--- src/services/s3Config.ts
+++ src/services/s3Config.ts
@@ -1,10 +1,10 @@
-// s3Config.ts
 import AWS from 'aws-sdk';
+import Config from 'react-native-config';
 
 AWS.config.update({
-  accessKeyId: 'YOUR_ACCESS_KEY_ID', // Replace with your AWS access key ID
-  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY', // Replace with your AWS secret access key
-  region: 'YOUR_AWS_REGION', // Replace with your AWS region
+  accessKeyId: Config.AWS_ACCESS_KEY_ID,
+  secretAccessKey: Config.AWS_SECRET_ACCESS_KEY,
+  region: Config.AWS_REGION,
 });
 
 const s3 = new AWS.S3();
@@ -13,7 +13,7 @@ export const getSignedUrl = async (bucketName: string, key: string, expires: num
   const params = {
     Bucket: bucketName,
     Key: key,
-    Expires: expires, // URL expiration time in seconds
+    Expires: expires,
   };
   return s3.getSignedUrlPromise('putObject', params);
 };
