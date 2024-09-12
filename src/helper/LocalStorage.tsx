e91f67dee8c097f27c67bc64566d72700ae28a4e
harshg-growwxxer <harah.gondaliya@growexx.com>
2024-09-12 16:46:48 +0530

    Added localstorage

---
+++ src/helper/LocalStorage.tsx
@@ -0,0 +1,31 @@
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class StorageHelper {
+  static async setItem(key: string, value: string): Promise<void> {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      console.error('AsyncStorage Error: ', error);
+    }
+  }
+
+  static async getItem(key: string): Promise<string | null> {
+    try {
+      return await AsyncStorage.getItem(key);
+    } catch (error) {
+      console.error('AsyncStorage Error: ', error);
+      return null;
+    }
+  }
+
+  static async removeItem(key: string): Promise<void> {
+    try {
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      console.error('AsyncStorage Error: ', error);
+    }
+  }
+}
+
+export default StorageHelper;
